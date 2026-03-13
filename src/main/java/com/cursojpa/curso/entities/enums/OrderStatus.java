7503c31740e6540fe5e4f070ffca97a27a9377e4
Vinicius Silveira <Vinisilveira.vianna@gmail.com>
2026-03-13 14:06:58 -0300

    OrderStatus Enum

---
+++ src/main/java/com/cursojpa/curso/entities/enums/OrderStatus.java
@@ -0,0 +1,31 @@
+package com.cursojpa.curso.entities.enums;
+
+public enum OrderStatus {
+
+	ESPERANDO_PAGAMENTO(1),
+	PAGO(2),
+	ENVIADO(3),
+	RECEBIDO(4),
+	CANCELADO(5);
+	
+	private int code;
+	
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	public static OrderStatus valueOf(int code) {
+		for (OrderStatus value : OrderStatus.values()) {
+			if (value.getCode() == code) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Código de Status Inválido.");
+	}
+	
+	
+}
